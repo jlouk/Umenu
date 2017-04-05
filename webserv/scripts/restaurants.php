7617148a68a942b95d70c5427142e974dc48dd4c
Scott Lacey <crimsonslace@gmail.com>
2017-04-04 22:30:46 -0500

    Added GeoLocation PHP class to include files; Updated restaurants.php to return restaurants sorted by distance from a specified lat,lon value.

--- webserv/scripts/restaurants.php
+++ webserv/scripts/restaurants.php
@@ -7,9 +7,29 @@ prints out all entries in the restaurant table as JSON array
 */
 
 include("../incl/config-inc.php");
+require_once("../incl/GeoLocation.php");
+
+$GeoLocation = 'AnthonyMartin\GeoLocation\GeoLocation';
+
 header('Content-Type: application/json');
 
-$qry = "SELECT * FROM `restaurants`";
+$lat = $_POST['lat'];
+$lon = $_POST['lon'];
+$dist = $_POST['dist'];
+
+// GeoLocation of provided lat,lon values
+$loc = $GeoLocation::fromDegrees($lat,$lon);
+$bounds = $loc->boundingCoordinates($dist,"mi");
+
+// Build SQL query
+$qry = "SELECT * FROM `restaurants` WHERE `latitude`>=" . $bounds[0]->getLatitudeInDegrees() . " AND `latitude`<=" . $bounds[1]->getLatitudeInDegrees();
+if($bounds[0]->getLongitudeInDegrees() > $bounds[1]->getLongitudeInDegrees())
+{
+	$qry = $qry . " AND (`longitude`>=" . $bounds[0]->getLongitudeInDegrees() . " OR `longitude`<=" . $bounds[1]->getLongitudeInDegrees() . ")";
+}else{
+	$qry = $qry . " AND `longitude`>=" . $bounds[0]->getLongitudeInDegrees() . " AND `longitude`<=" . $bounds[1]->getLongitudeInDegrees();
+}
+
 $result = mysqli_query($db,$qry);
 $count = mysqli_num_rows($result);
 $arr = array();
@@ -22,8 +42,20 @@ for($i=0; $i<$count; $i++)
 	$rowarr['restaurantName'] = $row['restaurantName'];
 	$rowarr['latitude'] = $row['latitude'];
 	$rowarr['longitude'] = $row['longitude'];
+	// Calculate distance of restaurant from user's location
+	$tempLoc = $GeoLocation::fromDegrees($rowarr['latitude'],$rowarr['longitude']);
+	$rowarr['distance'] = $loc->distanceTo($tempLoc,"mi");
+	
 	array_push($arr,$rowarr);
 }
+
+// Sort restaurants by distance from user
+$sort_col = array();
+foreach($arr as $innerArr)
+{
+	array_push($sort_col, $innerArr['distance']);
+}
+array_multisort($sort_col,$arr);
 
 echo(json_encode($arr));
 
