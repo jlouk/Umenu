9b8962c31ab0a2f947ec8a60eff0a50a8e028c70
jlouk <jlouk@wisc.edu>
2017-04-05 18:37:30 -0500

    Test commit adding functions to the scripts

--- webserv/scripts/getdishes.php
+++ webserv/scripts/getdishes.php
@@ -11,46 +11,38 @@ header('Content-Type: application/json');
 
 //http://stackoverflow.com/questions/15485354/angular-http-post-to-php-and-undefined
 $restaurantId = $_POST['restaurantId'];
-/*$postdata = file_get_contents("php://input");
-$request = json_decode($postdata);
-$restaurantId = $request->restaurantId;
+$result = $restaurantQuery($restaurantId);
+echo $result;
 
-if (isset($postdata)) {
-$request = json_decode($postdata);
-$username = $request->restaurantId;
+/**
+ * Function created by JL to make this more testable.  If problems, revert to prior commits
+ * @param $restaurantId
+ * @return string
+ */
 
-if ($username != "") {
-echo "Server returns: " . $username;
-}
-else {
-echo "Empty username parameter!";
-}
-}
-else {
-echo "Not called properly with username parameter!";
-}*/
-
-$qry = "SELECT * FROM `dishes` WHERE `restaurantId` = " . $restaurantId . " LIMIT 15";
-//$qry = "SELECT * FROM `dishes` WHERE `restaurantId` = 1";
-$result = mysqli_query($db,$qry);
-$count = mysqli_num_rows($result);
-$arr = array();
-
-for($i=0; $i<$count; $i++)
+function restaurantQuery($restaurantId)
 {
-	$row = mysqli_fetch_array($result);
-	$rowarr = array();
-	$rowarr['dishId'] = $row['dishId'];
-	$rowarr['categoryId'] = $row['categoryId'];
-	$rowarr['restaurantId'] = $row['restaurantId'];
-	$rowarr['dishName'] = $row['dishName'];
-	$rowarr['description'] = $row['description'];
-	$rowarr['price'] = $row['price'];
-	array_push($arr,$rowarr);
+    $qry = "SELECT * FROM `dishes` WHERE `restaurantId` = " . $restaurantId . " LIMIT 15";
+//$qry = "SELECT * FROM `dishes` WHERE `restaurantId` = 1";
+    $result = mysqli_query($db, $qry);
+    $count = mysqli_num_rows($result);
+    $arr = array();
+
+    for ($i = 0; $i < $count; $i++) {
+        $row = mysqli_fetch_array($result);
+        $rowarr = array();
+        $rowarr['dishId'] = $row['dishId'];
+        $rowarr['categoryId'] = $row['categoryId'];
+        $rowarr['restaurantId'] = $row['restaurantId'];
+        $rowarr['dishName'] = $row['dishName'];
+        $rowarr['description'] = $row['description'];
+        $rowarr['price'] = $row['price'];
+        array_push($arr, $rowarr);
+    }
+
+    mysqli_close($db);
+    return (json_encode($arr));
+
 }
-
-echo(json_encode($arr));
-
-mysqli_close($db);
 
 ?>
