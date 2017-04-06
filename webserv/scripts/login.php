360c55c170bde41066dff74507eee8b1a43b4cdd
jlouk <jlouk@wisc.edu>
2017-04-06 10:25:38 -0500

    Fixing typo in login.php

--- webserv/scripts/login.php
+++ webserv/scripts/login.php
@@ -12,7 +12,7 @@ header('Content-Type: application/json');
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query = "SELECT * FROM `users` WHERE username =  $username AND password = $passwordd;";
+$query = "SELECT * FROM `users` WHERE username =  $username AND password = $password;";
 
 $result = mysqli_query($db, $query);
 $arr = array();
