1091ca1c1fd04e86306e4efb328fd02bb8dec857
jlouk <jlouk@wisc.edu>
2017-04-05 20:07:06 -0500

    Reverting my saucy changes for continual functionality.  Stay woke friends

--- webserv/scripts/getmencat.php
+++ webserv/scripts/getmencat.php
@@ -1,35 +1,24 @@
 <?php
-
 /*
 restaurants.php
-
 prints out all entries in the restaurant table as JSON array
 */
-
 include("../incl/config-inc.php");
 header('Content-Type: application/json');
-
 $restaurantId = $_POST['restaurantId'];
-$results = restaurantQuery($restaurantId);
-echo $results;
-
-
-function restaurantQuery($restaurantId)
+$qry = "SELECT * FROM `menu_categories` WHERE `restaurantId` = " . $restaurantId;
+$result = mysqli_query($db,$qry);
+$count = mysqli_num_rows($result);
+$arr = array();
+for($i=0; $i<$count; $i++)
 {
-    $qry = "SELECT * FROM `menu_categories` WHERE `restaurantId` = " . $restaurantId;
-    $result = mysqli_query($db, $qry);
-    $count = mysqli_num_rows($result);
-    $arr = array();
-
-    for ($i = 0; $i < $count; $i++) {
-        $row = mysqli_fetch_array($result);
-        $rowarr = array();
-        $rowarr['categoryId'] = $row['categoryId'];
-        $rowarr['categoryName'] = $row['categoryName'];
-        $rowarr['restaurantId'] = $row['restaurantId'];
-        array_push($arr, $rowarr);
-    }
-    mysqli_close($db);
-    return (json_encode($arr));
+	$row = mysqli_fetch_array($result);
+	$rowarr = array();
+	$rowarr['categoryId'] = $row['categoryId'];
+	$rowarr['categoryName'] = $row['categoryName'];
+	$rowarr['restaurantId'] = $row['restaurantId'];
+	array_push($arr,$rowarr);
 }
+echo(json_encode($arr));
+mysqli_close($db);
 ?>
