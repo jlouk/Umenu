06178e4590dd74e934a9fe49bdcc63642f2fcd45
Scott Lacey <crimsonslace@gmail.com>
2017-04-05 20:23:13 -0500

    Updated scripts to interpret JSON input data from Ionic

--- webserv/scripts/getmencat.php
+++ webserv/scripts/getmencat.php
@@ -9,24 +9,30 @@ prints out all entries in the restaurant table as JSON array
 include("../incl/config-inc.php");
 header('Content-Type: application/json');
 
-$restaurantId = $_POST['restaurantId'];
+$postdata = file_get_contents("php://input");
+if(isset($postdata)) {
+	$request = json_decode($postdata);
+	$restaurantId = $request->restaurantId;
 
-$qry = "SELECT * FROM `menu_categories` WHERE `restaurantId` = " . $restaurantId;
-$result = mysqli_query($db,$qry);
-$count = mysqli_num_rows($result);
-$arr = array();
-
-for($i=0; $i<$count; $i++)
-{
-	$row = mysqli_fetch_array($result);
-	$rowarr = array();
-	$rowarr['categoryId'] = $row['categoryId'];
-	$rowarr['categoryName'] = $row['categoryName'];
-	$rowarr['restaurantId'] = $row['restaurantId'];
-	array_push($arr,$rowarr);
+	$qry = "SELECT * FROM `menu_categories` WHERE `restaurantId` = " . $restaurantId;
+	$result = mysqli_query($db,$qry);
+	$count = mysqli_num_rows($result);
+	$arr = array();
+	
+	for($i=0; $i<$count; $i++)
+	{
+		$row = mysqli_fetch_array($result);
+		$rowarr = array();
+		$rowarr['categoryId'] = $row['categoryId'];
+		$rowarr['categoryName'] = $row['categoryName'];
+		$rowarr['restaurantId'] = $row['restaurantId'];
+		array_push($arr,$rowarr);
+	}
+	
+	echo(json_encode($arr));
+} else {
+	echo("POST DATA NOT AVAILABLE");
 }
-
-echo(json_encode($arr));
 
 mysqli_close($db);
 
