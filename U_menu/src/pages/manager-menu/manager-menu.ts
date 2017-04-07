1cbeb08538c3210fd3acbfdeef02da77663596a7
Daniel Mackenthun <dmackenthun@wisc.edu>
2017-04-06 20:01:34 -0500

    delete functionality completed and added to project

--- U_menu/src/pages/manager-menu/manager-menu.ts
+++ U_menu/src/pages/manager-menu/manager-menu.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { AuthService } from '../../providers/auth-service';
 import { ManagerDishDetails } from '../manager-dishdetails/manager-dishdetails';
+//import { Dialogs } from '@ionic-native/dialogs';
+
 
 
 @Component({
@@ -19,9 +21,11 @@ export class ManagerMenuPage {
   restaurant: any;
   loggedInUser: any;
   editData: any;
+  deleteResponse: any;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http,  private auth: AuthService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+              public http: Http,  private auth: AuthService) {
     // If we navigated to this page, we will have an item available as a nav param
     this.loggedInUser = navParams.get('user');
     this.postdata = {restaurantId: -1};
@@ -107,6 +111,31 @@ export class ManagerMenuPage {
       dish: this.editData,
       user: this.loggedInUser
     });
+  }
+
+  deleteDish(dishToDelete) {
+      let body   : string	 = JSON.stringify({userId: this.loggedInUser[0].userId, dishId: dishToDelete.dishId}),
+          type 	 : string	 = "application/x-www-form-urlencoded; charset=UTF-8",
+          headers: any		 = new Headers({ 'Content-Type': 'application/json'}),
+          options: any 		 = new RequestOptions({ headers: headers }),
+          url 	 : any		 = "http://s673534317.onlinehome.us/scripts/deleteDish.php";
+
+      this.http.post(url, body, options)
+      .map(res => res.json())
+      .subscribe((data) =>
+      {
+         // If the request was successful notify the user
+         if(data.valid == 1)
+         {
+           console.log("delete was successful");
+           this.refreshPage();
+         }
+         // Otherwise let 'em know anyway
+         else
+         {
+           console.log("delete was unsuccessful");
+         }
+      });
   }
 
   refreshPage() {
