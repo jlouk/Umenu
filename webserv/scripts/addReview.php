3440e1f42317f501c42cffeeb83b0ed0265ceb76
jlouk <jlouk@wisc.edu>
2017-05-04 00:03:51 -0500

    finalizing addreview

--- webserv/scripts/addReview.php
+++ webserv/scripts/addReview.php
@@ -13,27 +13,30 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 
 $dishId = $_POST['dishId'];
 $rating = $_POST['rating'];
-
+$value = [];
 /*$testQuery = "SELECT * FROM reviews where userId = \"$userId\" and dishId = \"$dishId;";
 $testResult = mysqli_query($db,$testQuery);*/
 
+
 $query = "SELECT * FROM dishes where dishId = $dishId;";
 $results = mysqli_query($db, $query);
-$item = '';
+$item = [];
 while ($data = mysqli_fetch_assoc($results)){
     $item = $data;
     break;
 }
-
-if ($item){
-    $numRatings = $item['numRatings'] + 1;
-    $average = ($rating + $item['rating']) / $numRatings;
-    $sql = "UPDATE dishes SET rating = $average WHERE dishId = $dishId;";
+if ($item['dishId']){
+    $numRatings = $item['numRatings'];
+    $average = (($item['rating'] * $numRatings) + $rating) / ($numRatings + 1);
+    $numRatings= $numRatings+1;
+    $sql = "UPDATE dishes SET rating = $average, numRatings = $numRatings WHERE dishId = $dishId;";
     mysqli_query($db,$sql);
-    echo "1";
+    $value['average'] = $average;
+    $value['numRatings'] = $numRatings;
+    $value['valid'] = 1;
 }
 else {
-    echo "0";
+    $value['valid'] = 0;
 }
-
+echo json_encode($value);
 ?>
