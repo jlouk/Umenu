ff95bd982fe30024d49a74f256f545ccaf070ec2
jlouk <jlouk@wisc.edu>
2017-05-04 11:11:20 -0500

    regular expressions with dishes

--- webserv/scripts/getdishes.php
+++ webserv/scripts/getdishes.php
@@ -22,14 +22,26 @@ if(isset($postdata)) {
 	for($i=0; $i<$count; $i++)
 	{
 		$row = mysqli_fetch_array($result);
+
 		$rowarr = array();
 		$rowarr['dishId'] = $row['dishId'];
 		$rowarr['categoryId'] = $row['categoryId'];
 		$rowarr['restaurantId'] = $row['restaurantId'];
-		$rowarr['dishName'] = $row['dishName'];
-		$rowarr['description'] = $row['description'];
-		$rowarr['price'] = $row['price'];
-		$rowarr['currRating'] = $row['currRating'];
+
+		$tempName = str_replace('&amp;','&',$row['dishName']);
+		$tempName = str_replace('&comma;',',', $tempName);
+		$tempName = str_replace('&apos;','\'', $tempName);
+		$rowarr['dishName'] = $tempName;
+
+
+        $tempDes = str_replace('&amp;','&',$row['description']);
+        $tempDes = str_replace('&comma;',',', $tempDes);
+        $tempDes = str_replace('&apos;','\'', $tempDes);
+        $rowarr['description'] = $tempDes;
+
+
+        $rowarr['price'] = $row['price'];
+		$rowarr['rating'] = $row['rating'];
 		$rowarr['numRatings'] = $row['numRatings'];
 		array_push($arr,$rowarr);
 	}
